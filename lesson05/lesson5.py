79bb8294ecbd0679304ad7135b5d1399abcad4f0
Bailey <bailey@218r02497r87dh2.edu.yrdsb.net>
2025-02-20 09:32:12 -0500

    solved lesson 5

--- lesson05/lesson5.py
+++ lesson05/lesson5.py
@@ -1,1 +1,16 @@
 # Lesson 5
+start_money = float(input('Enter initial ammount: ')) 
+cookies_sold = input("Enter combination of cookies: ")
+
+# processing
+
+big_cookies = cookies_sold.count("b")
+cookies = cookies_sold.count("c")
+
+total_cookies = big_cookies + cookies
+profit = ((big_cookies * 2) + (cookies * 1.25)) - ((big_cookies * 0.75) + (cookies * 0.5))
+end_day = start_money + profit
+
+# output
+
+print(f'We sold {total_cookies} cookies. We made ${profit}. We now have ${end_day}.')
