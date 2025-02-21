555312a1b0893a8831ff147f8a3c0a504644571a
Bailey <bailey@SEA-91DZ3L2.EDU.YRDSB.NET>
2025-02-21 10:35:23 -0500

    solved lesson 10

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -1,1 +1,16 @@
 # Lesson 10
+
+# input
+number = input("Enter the last 4 digits of the number: ")
+
+# processing and output
+if number[0] == "8" or number[1] == "9":
+    if number[1] == number[2]:
+        if number[3] == "8" or "9":
+            print("ignore")
+        else: 
+            print("answer")
+    else:
+        print("answer")
+else:
+    print("answer")
