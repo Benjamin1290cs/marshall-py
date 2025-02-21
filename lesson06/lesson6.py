9d21ec03533de8ad233839b835d84c8cbbdbf669
Bailey <bailey@Maxs-MacBook-Air.local>
2025-02-20 21:36:37 -0500

    solved lesson 6

--- lesson06/lesson6.py
+++ lesson06/lesson6.py
@@ -1,1 +1,15 @@
 # Lesson 6
+name = input("Enter your name: ")
+if not name:
+    print("Name was not given")
+else: 
+    print(f"Hello, {name}!")
+
+if "a" in "Jasper":
+    print("a is found in Jasper.")
+
+if "Apple" not in ["Kiwi", "Strawberry", "Watermelon"]
+    print("Apple was not found in our list of fruits.")
+
+ if "water" in "watermellon":
+    print("water exists inside watermelon.")
