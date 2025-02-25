abf72c9a74e92ed697275b50716169a461828377
Bailey <bailey@TEACH2D-H5B99S3.EDU.YRDSB.NET>
2025-02-25 09:15:33 -0500

    solved lesson 11

--- lesson11/lesson11.py
+++ lesson11/lesson11.py
@@ -1,1 +1,21 @@
 # Lesson 11
+
+# input
+
+points = input("Enter x and y values: ")
+points = points.split(" ")
+points = list(map(int, points))
+x, y = points
+print(f"The x-value is {x}.")
+print(f"The y-value is {y}.")
+
+if x < 0:
+    if y > 0:
+        print("Quadrant 2")
+    else:
+        print("Quadrant 3")
+elif x > 0:
+    if y > 0:
+        print("Quadrant 1")
+    else: 
+        print("Quadrant 4")
