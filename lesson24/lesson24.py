bf67403338ffb1bb442ffb1729f37f00619d0391
Bailey <bailey@SEA-JYQ33D3.EDU.YRDSB.NET>
2025-02-27 10:33:45 -0500

    solved lesson24

--- lesson24/lesson24.py
+++ lesson24/lesson24.py
@@ -1,1 +1,17 @@
 # Lesson 24
+loop = True
+max_name = ""
+
+while loop:
+    user_input = input("Enter a name or enter \'X\' to exit: ")
+    if user_input.lower().capitalize() == "X":
+        loop = False
+        break
+    else:
+        if len(user_input) > len(max_name):
+            max_name = user_input
+
+if max_name == "":
+    print("Insufficient data.")
+else:
+    print(max_name)
