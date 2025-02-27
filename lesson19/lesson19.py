5133ae325ef3271f2f8bc7d0534ca12be564cbca
Bailey <bailey@Maxs-MacBook-Air.local>
2025-02-26 22:28:47 -0500

    solved lesson 19

--- lesson19/lesson19.py
+++ lesson19/lesson19.py
@@ -1,1 +1,16 @@
 # Lesson 19
+
+# input 
+num = int(input("Enter a number: "))
+
+# processing
+counter = 0
+for divider in range(1, num+1):
+    if num % divider == 0:
+        counter += 1
+
+# output
+if counter == 2:
+    print(f"{num} is a prime number.")     
+else: 
+    print(f"{num} is a composite number.") 
