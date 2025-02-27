449fb2cf781de6ec855bd7a06c861cbbc2ee6d3a
Bailey <bailey@Maxs-MacBook-Air.local>
2025-02-26 21:44:17 -0500

    solved lesson 18

--- lesson18/lesson18.py
+++ lesson18/lesson18.py
@@ -1,1 +1,9 @@
 # Lesson 18
+
+# input 
+num = int(input("Enter a number: "))
+
+# processing
+for divider in range(1, num+1):
+    if num % divider == 0:
+        print(f"{divider} is a factor of {num}.")
