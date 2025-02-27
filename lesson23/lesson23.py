01f96c8d9356b72a1dbc901cde37229a15966c0a
Bailey <bailey@SEA-JYQ33D3.EDU.YRDSB.NET>
2025-02-27 10:22:23 -0500

    solved lesson 23

--- lesson23/lesson23.py
+++ lesson23/lesson23.py
@@ -1,1 +1,26 @@
 # Lesson 23
+
+loop = True
+counter = 0
+total_sum = 0
+
+while loop:
+    user_input = input("Enter the mark or \"Exit\" to exit: ")
+    if user_input.lower().capitalize() == "Exit":
+        loop = False
+        break
+    else:
+        mark = int(user_input)
+        if 0 <= mark <= 100:
+            total_sum += mark
+            counter += 1
+        else:
+            print("Invalid input.")
+
+average = total_sum / counter
+
+print(average)
+
+
+
+
