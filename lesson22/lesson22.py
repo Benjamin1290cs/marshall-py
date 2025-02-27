bfac0929eb1b04aa09c6baab587758f1621275e3
Bailey <bailey@SEA-JYQ33D3.EDU.YRDSB.NET>
2025-02-27 09:48:26 -0500

    solved lesson 22

--- lesson22/lesson22.py
+++ lesson22/lesson22.py
@@ -1,1 +1,14 @@
 # Lesson 22
+
+upper_limit = int(input("Enter N to find the Nth Fibonacci number: "))
+
+fib_0 = 0
+fib_1 = 1
+fib_n = 0
+
+for n in range (2, upper_limit+1):
+    fib_n = fib_0 + fib_1
+    fib_0 = fib_1
+    fib_1 = fib_n
+
+print(f"{fib_n} is {upper_limit}'s Nth Fibonacci number.")
