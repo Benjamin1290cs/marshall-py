c6a0002294e79d5f3b87bd16d990fde93318921f
Bailey <bailey@Maxs-MacBook-Air.local>
2025-02-26 19:49:58 -0500

    solved lesson 12

--- lesson12/lesson12.py
+++ lesson12/lesson12.py
@@ -1,1 +1,19 @@
 # Lesson 12
+
+# input
+angle1 = int(input("Enter angle 1: "))
+angle2 = int(input("Enter angle 2: "))
+angle3 = int(input("Enter angle 3: "))
+
+# processing and output
+angle_sum = angle1 + angle2 + angle3
+
+if angle_sum != 180:
+    print("Invalid angles.")
+else:
+    if angle1 == angle2 == angle3:
+        print("Equilateral triangle.")
+    elif angle1 == angle2 or angle1 == angle3 or angle2 == angle3:
+        print("Isoceles triangle.")
+    else:
+        print("Scalene triangle.")
