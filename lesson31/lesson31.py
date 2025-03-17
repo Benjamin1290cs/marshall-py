ab8730c6b0b9b5acad34d5c782bfa42c2f60e58f
Bailey <bailey@218seaxkfmxg612.edu.yrdsb.net>
2025-03-17 10:59:27 -0400

    solved lesson 31

--- lesson31/lesson31.py
+++ lesson31/lesson31.py
@@ -1,1 +1,18 @@
 # Lesson 31
+
+def is_anagram(word1, word2):
+    if len(word1) != len(word2):
+        return False
+    else: 
+        for character in word1:
+            if word1.count(character) != word2.count(character):
+                return False
+    return True
+
+word1 = input("Enter text 1: ")
+word2 = input("Enter text 2: ")
+
+if is_anagram(word1, word2):
+    print("True")
+else:
+    print("False")
