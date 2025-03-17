858bb839e7c38ba6effdc235f8c82e3f54c01657
Bailey <bailey@Maxs-MacBook-Air.local>
2025-03-16 22:47:58 -0400

    solved lesson 29

--- lesson29/lesson29.py
+++ lesson29/lesson29.py
@@ -1,1 +1,14 @@
 # Lesson 29
+
+def consonant_finder(text):
+    text = text.lower()
+    counter = 0
+    for character in text:
+        if character.isalpha() and character not in ["a", "e", "i", "o", "u"]:
+            counter += 1
+
+    return counter
+
+text = input("Enter a text: ")
+result = consonant_finder(text)
+print(f"There is {result} consonants in {text}")
