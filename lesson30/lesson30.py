18474f4eef2686460b20d24f9177cbd5d72ec8a1
Bailey <bailey@218seaxkfmxg612.edu.yrdsb.net>
2025-03-17 10:20:40 -0400

    solved lesson 30

--- lesson30/lesson30.py
+++ lesson30/lesson30.py
@@ -1,1 +1,19 @@
 # Lesson 30
+
+def create_line(num):
+    result = ""
+    for i in range(1, num+1):
+        if i % 2 == 0:
+            result += "0"
+        else:
+            result += "1"
+    
+    return result
+
+def output_pattern(num):
+    for i in range(1, num+1):
+        print(create_line(i))
+
+num = int(input("Enter a num: "))
+output_pattern(num)
+
