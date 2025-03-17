86360dbdf453fad1189ec5dd0073e290b64b82ca
Bailey <bailey@Maxs-MacBook-Air.local>
2025-03-16 22:26:40 -0400

    solved lesson 27

--- lesson27/lesson27.py
+++ lesson27/lesson27.py
@@ -1,1 +1,14 @@
 # Lesson 27
+
+def string_cleaner(text):
+    cleaned = ""
+    for character in text:
+        if character.isalpha():
+            cleaned += character.lower()
+        
+    return cleaned
+
+text = input("Enter text: ")
+result = string_cleaner(text)
+
+print(result)
