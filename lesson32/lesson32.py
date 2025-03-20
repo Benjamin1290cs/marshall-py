cd4ffa64ba2acb367d175946c30560f20cd1e7cd
Bailey <bailey@SEA-DQJH3M3.EDU.YRDSB.NET>
2025-03-20 14:00:49 -0400

    solved lesson 32

--- lesson32/lesson32.py
+++ lesson32/lesson32.py
@@ -1,1 +1,15 @@
 # Lesson 32
+
+def duplicates(word1, word2):
+    if not word1 or not word2:
+        return[]
+    else:
+        set_word1 = set(word1)
+        set_word2 = set(word2)
+        result = []
+        for character in set_word1:
+            if character in set_word2:
+                result.append(character)
+        return sorted(result)
+    
+print(duplicates("hello, world", "goodbye world"))
