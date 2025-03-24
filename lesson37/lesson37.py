908f4dd1a545caa2f36e24d3f66338bc6003c2c9
Bailey <bailey@223SEAXK9QWG612.EDU.YRDSB.NET>
2025-03-24 12:14:16 -0400

    solved lesson 37

--- lesson37/lesson37.py
+++ lesson37/lesson37.py
@@ -1,1 +1,20 @@
 # Lesson 37
+
+def string_comp(text):
+    if not text:  
+        return ""
+    
+    ctr = 1
+    result = ""
+    
+    for character in range(1, len(text)):
+        if text[character] == text[character - 1]:
+            ctr += 1
+        else:
+            result += f"{text[character - 1]}{ctr}"
+            ctr = 1
+            
+    result += f"{text[-1]}{ctr}" 
+    return result
+
+print(string_comp("aabcccccaaa"))
