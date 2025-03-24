9108292280df91c06105b0330994c1315d20b812
Bailey <bailey@Maxs-MacBook-Air.local>
2025-03-23 20:19:07 -0400

    solved lesson 35

--- lesson35/lesson35.py
+++ lesson35/lesson35.py
@@ -1,1 +1,12 @@
 # Lesson 35
+
+def dupe_remover(a_list):
+    new_list = []
+    for item in a_list:
+        if item not in new_list:
+            new_list.append(item)
+    return new_list
+
+test = ["a","b","c","c","b","c","a", "a", "d"]
+print(f"test list: {test}")
+print(f"duplicates removed: {dupe_remover(test)}")
