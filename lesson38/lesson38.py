0b1d517c58fe28b48b88269e351a85d56829bf7e
Bailey <bailey@223SEAXK9QWG612.EDU.YRDSB.NET>
2025-03-24 14:13:40 -0400

    solved lesson 38

--- lesson38/lesson38.py
+++ lesson38/lesson38.py
@@ -1,1 +1,15 @@
 # Lesson 38
+
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
+
+def largest_palindrome_product():
+    max_palindrome = 0
+    for num1 in range(999, 99, -1):
+        for num2 in range(num1, 99, -1):  
+            product = num1 * num2
+            if is_palindrome(product) and product > max_palindrome:
+                max_palindrome = product
+    return max_palindrome
+
+print(largest_palindrome_product())
