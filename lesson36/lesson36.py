3ab5d5e09bcf5fa097a57cb47e0bb80b5a5ae1bd
Bailey <bailey@Maxs-MacBook-Air.local>
2025-03-23 20:28:12 -0400

    solved lesson 36

--- lesson36/lesson36.py
+++ lesson36/lesson36.py
@@ -1,1 +1,16 @@
 # Lesson 36
+
+def factor_finder(num):
+    result = []
+    for divider in range(1, num+1):
+        if num % divider == 0:
+            result.append(divider)
+    return result
+
+def is_prime(num):
+    return len(factor_finder(num)) == 2
+
+print(f"Factors of 13: {factor_finder(13)}")
+print(f"Factors of 36: {factor_finder(36)}")
+print(f"Is 36 prime?: {is_prime(36)}")
+print(f"Is 13 prime?: {is_prime(13)}")
