35a2a22692184a48975f414a6b6aad6706b957cc
Bailey <bailey@251seaxk5mx6n52.edu.yrdsb.net>
2025-03-25 08:42:09 -0400

    solved lesson 40

--- lesson40/lesson40.py
+++ lesson40/lesson40.py
@@ -1,1 +1,24 @@
 # Lesson 40
+
+def factor_finder(num):
+    result = []
+    for divider in range(1,num+1):
+        if num % divider == 0:
+            result.append(divider)
+    return result
+
+def is_prime(num):
+    return len(factor_finder(num)) == 2
+
+def prime_list(num):
+    primes = [2]
+    if num == 2:
+        return primes
+    else:
+        for num in range(3, num, 2):
+            if is_prime(num):
+                primes.append(num)
+        return primes
+
+print(prime_list(100))
+
