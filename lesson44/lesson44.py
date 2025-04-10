086180f65bfa23e041cd1730a04cbd92970e2ca5
Bailey <bailey@Maxs-MacBook-Air.local>
2025-04-10 00:27:51 -0400

    solved lesson 44

--- lesson44/lesson44.py
+++ lesson44/lesson44.py
@@ -1,1 +1,15 @@
 # Lesson 44
+
+def c_frequency(text):
+    result = {}
+    clean_word = sorted(text.lower())
+
+    for character in clean_word:
+        if character not in result:
+            result[character] = 1
+        else:
+            result[character] += 1
+    return result
+
+print(c_frequency("hello"))
+
