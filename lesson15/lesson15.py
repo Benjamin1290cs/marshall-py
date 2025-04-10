c9a3b91d2a098acf925ab8860460b46d60202abd
Bailey <bailey@218L0MBL9214W89.EDU.YRDSB.NET>
2025-04-10 14:09:53 -0400

    solved lesson 15

--- lesson15/lesson15.py
+++ lesson15/lesson15.py
@@ -1,1 +1,28 @@
 # Lesson 15
+
+condition_variable = True
+password = "poop"
+
+while condition_variable:
+    user_input = input("Guess the password: ")
+
+    if user_input == "poop":
+        print("Correct guess!")
+        condition_variable = False
+    else:
+        print("Incorrect guess!")
+
+print("\n")
+
+user_input = input("Search for a fruit:")
+
+fruits = ["Apple", "Banana", "Kiwi", "Strawberry"]
+found = False
+
+for fruit_name in fruits:
+    if fruit_name == user_input:
+        print(f"{user_input} is found!")
+        found = True
+
+if not found: 
+    print(f"{user_input} is not found...")
